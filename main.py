000b865553f5070a30adf35bfbd69b50b7d1305c
mrgick <mrgigant55555@mail.ru>
2021-03-09 16:56:32 +0300

    add find

--- main.py
+++ main.py
@@ -48,18 +48,27 @@ def week_tests(n=0):
 	number=str(int(time.mktime(moday_zero.timetuple())))
 	return time_now,monday,moday_zero,number
 """
+#поиск определенного значения по ключу name
+async def do_find_all_names(db="DB",collection="rasp"):
+	db = client[db]
+	collection = db[collection]
+	all_names=[]
+	async for document in collection.find({}):
+		all_names.append(document['name'])
+	return all_names
+
 
 #Хранилице данных, которые берутся несколько раз
 storage = Storage()
 async def initialize_storage():
 	all_dict = Key("ALL")
-	ALL={'0431-01': 'Inst6_0431_01', '0022-01': 'Inst6_0022_01', '0023-01': 'Inst6_0023_01', '0024-01': 'Inst6_0024_01', '0431-02': 'Inst6_0431_02', '0022-02': 'Inst6_0022_02', '0023-02': 'Inst6_0023_02', '0024-02': 'Inst6_0024_02', '0431-03': 'Inst6_0431_03', '0022-03': 'Inst6_0022_03', '0023-03': 'Inst6_0023_03', '0024-03': 'Inst6_0024_03', '0431-04': 'Inst6_0431_04', '0022-04': 'Inst6_0022_04', '0023-04': 'Inst6_0023_04', '0024-04': 'Inst6_0024_04', '0431-05': 'Inst6_0431_05', '0022-05': 'Inst6_0022_05', '0023-05': 'Inst6_0023_05', '0024-05': 'Inst6_0024_05', '0431-06': 'Inst6_0431_06', '0022-06': 'Inst6_0022_06', '0023-06': 'Inst6_0023_06', '0024-06': 'Inst6_0024_06', '0431-07': 'Inst6_0431_07', '0032-01': 'Inst6_0032_01', '0033-01': 'Inst6_0033_01', '0034-01': 'Inst6_0034_01', '0431-08': 'Inst6_0431_08', '0032-03': 'Inst6_0032_03', '0033-02': 'Inst6_0033_02', '0034-03': 'Inst6_0034_03', '0431-09': 'Inst6_0431_09', '0032-04': 'Inst6_0032_04', '0033-04': 'Inst6_0033_04', '0034-04': 'Inst6_0034_04', '0431-11': 'Inst6_0431_11', '0032-05': 'Inst6_0032_05', '0033-05': 'Inst6_0033_05', '0034-05': 'Inst6_0034_05', '0431-13': 'Inst6_0431_13', '0032-06': 'Inst6_0032_06', '0033-08': 'Inst6_0033_08', '0034-06': 'Inst6_0034_06', '0431-03М': 'Inst6_0431_03М', '0032-08': 'Inst6_0032_08', '0034-08': 'Inst6_0034_08', '0431-08М': 'Inst6_0431_08М', '0032-09': 'Inst6_0032_09', '0034-09': 'Inst6_0034_09', '0431-09М': 'Inst6_0431_09М', '0022-09М': 'Inst6_0022_09М', '0431-11М': 'Inst6_0431_11М', '0032-07М': 'Inst6_0032_07М', '2431-03М': 'Inst6_2431_03М', '2032-01М': 'Inst6_2032_01М', '2431-09М': 'Inst6_2431_09М'}
+	ALL= await do_find_all_names()
 	await storage.put(all_dict, ALL)
 
 #поиск определенного значения по ключу name
-async def db_find_name(name):
-	db = client['DB']
-	collection = db['rasp']
+async def db_find_name(name, db="DB",collection="rasp"):
+	db = client[db]
+	collection = db[collection]
 	values = await collection.find_one({'name': name})
 	return values 
 
@@ -89,8 +98,8 @@ async def handle(event: bot.SimpleBotEvent) -> str:
 	#Проверяем есть ли аргументы
 	if len(args)>0:
 		#проверяем первый аргумент (имя)
-		ALL=await storage.get(Key("ALL"))
-		if ALL.get(args[0])!=None:
+		all_names = await storage.get(Key("ALL"))
+		if (args[0] in all_names)==True:
 			data = await db_find_name(args[0])
 			data=data.get('text')
 			
@@ -118,6 +127,26 @@ async def handle(event: bot.SimpleBotEvent) -> str:
 			"""					
 	await event.answer(message=mess)
 
+
+#поиск имени
+@bot.message_handler(bot.command_filter(commands=("find", "поиск"), prefixes=("/")))
+async def handle(event: bot.SimpleBotEvent) -> str:
+	args = event.object.object.message.text.split()[1:]
+	#Проверяем есть ли аргументы
+	if len(args)>0:
+		all_names = await storage.get(Key("ALL"))
+		message="похожие записи в базе данных:\n"
+		#ищем подстроку в строке в массиве
+		for x in all_names:
+			if x.find(args[0])!=-1:
+				message=message+x+"\n"
+			#чтобы не получить ошибку(макс лимит сообщения), делаем не больше 500
+			if (len(message))>=500:
+				message=message[0:message.rfind('\n')]
+				break
+	else:
+		message="вы не указали аргумент(имя)"		
+	await event.answer(message=message)
 
 
 
