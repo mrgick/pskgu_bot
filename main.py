697f2caac24e342d6deb2964ff031bcb1c9abc61
mrgick <mrgigant55555@mail.ru>
2021-03-11 09:47:34 +0300

    add test parser function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 from vkwave.bots import SimpleLongPollBot
-
-#для хранилища
 import asyncio
+#для хранилища
 from utils.storage import storage, initialize_storage
-
+from utils.parser import parser
 from blueprints import (
 	begin_router,
 	help_router,
@@ -21,6 +20,16 @@ bot.dispatcher.add_router(week_router)
 bot.dispatcher.add_router(help_router)
 bot.dispatcher.add_router(begin_router)
 
+
+
 if __name__=="__main__":
-	asyncio.get_event_loop().run_until_complete(initialize_storage())
-	bot.run_forever()
+	#создаем loop
+	loop=asyncio.get_event_loop()
+	#создаем хранилище
+	loop.run_until_complete(initialize_storage())
+	#запускаем бота
+	loop.create_task(bot.run(ignore_errors=True))
+	#запускаем парсер
+	loop.create_task(parser())
+	#заставляем работать вечно
+	loop.run_forever()
---
+++ utils/parser.py
@@ -0,0 +1,6 @@
+import asyncio
+#для теста сделан парсер, в дальнейшем переделать
+async def parser():
+	for k in range(100): 
+		#print(k)
+		await asyncio.sleep(1)
