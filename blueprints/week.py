611ae64ed7ef2ee47202bd677a5bd58f7350c013
mrgick <mrgickcool@gmail.com>
2021-03-28 17:33:30 +0300

    test

--- blueprints/week.py
+++ blueprints/week.py
@@ -71,7 +71,8 @@ async def handle(event: SimpleBotEvent) -> str:
             
             #получаем текст недели
             t=week_time(n)
-            message=readable_text(data,t) + t
+            time_now = datetime.datetime.now(pytz.timezone('Etc/GMT'))
+            message=readable_text(data,t) + time_now
 
 
 
