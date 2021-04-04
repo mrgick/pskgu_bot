ceca97d8968222d7411fe7331d5c6de8b6290d08
mrgick <mrgickcool@gmail.com>
2021-04-04 18:38:19 +0300

    refactoring

--- blueprints/map.py
+++ blueprints/map.py
@@ -3,23 +3,16 @@ from vkwave.bots import (
     simple_bot_message_handler,
     SimpleBotEvent,
     CommandsFilter,
+    PhotoUploader
 )
-
-from vkwave.api import Token, BotSyncSingleToken, API
-from vkwave.bots import PhotoUploader
-from vkwave.client import AIOHTTPClient
-import os
-
-client = AIOHTTPClient()
-api = API(clients=client, tokens=BotSyncSingleToken(Token(os.environ.get('TOKEN_VK'))),)
-uploader = PhotoUploader(api.get_context())
+from config import bot
 
 map_router = DefaultRouter()
 
 @simple_bot_message_handler(map_router, CommandsFilter(commands=("map", "карта"), prefixes=("/")))
 async def help(event: SimpleBotEvent) -> str:
     peer_id=event.object.object.message.peer_id
-    big_attachment = await uploader.get_attachments_from_links(
+    big_attachment = await PhotoUploader(bot.api_session.api.get_context()).get_attachments_from_links(
         peer_id=peer_id,
         links=[
             "https://cdn.discordapp.com/attachments/824647324981657620/825467801774718986/SPOILER_1etage.png",
@@ -28,5 +21,5 @@ async def help(event: SimpleBotEvent) -> str:
             "https://cdn.discordapp.com/attachments/824647324981657620/825467882536042516/SPOILER_4etage.png",
         ],
     )
-    await api.get_context().messages.send(user_id=peer_id, attachment=big_attachment, random_id=0)
+    await bot.api_session.api.get_context().messages.send(user_id=peer_id, attachment=big_attachment, random_id=0)
 
