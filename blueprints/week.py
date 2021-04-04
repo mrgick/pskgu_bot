b94f5f90241390889df416edadc3cc9d92cc4f0e
mrgick <mrgickcool@gmail.com>
2021-04-04 21:29:37 +0300

    refactoring

--- blueprints/week.py
+++ blueprints/week.py
@@ -4,19 +4,16 @@ from vkwave.bots import (
     SimpleBotEvent,
     CommandsFilter,
 )
-#for storage
 from vkwave.bots.storage.types import Key
 from utils.storage import storage
 from utils.web_db import db_find_name
-#for translations
-import googletrans
-#for time
 import datetime
+import googletrans
 
 week_router = DefaultRouter()
 
 
-#вывод недели   
+# Вывод имени   
 @simple_bot_message_handler(week_router, CommandsFilter(commands=("show", "показать"), prefixes=("/")))
 async def handle(event: SimpleBotEvent) -> str:
 
@@ -32,8 +29,8 @@ async def handle(event: SimpleBotEvent) -> str:
         return str(timestamp).split(".")[0]
 
     # Преобразуем словарь недели в читаемый вид
-    def readable_text(arr,time):
-        text = arr["weeks"].get(time)
+    def readable_text(data,time):
+        text = data["weeks"].get(time)
         if text != None:
             week = ""
             days_name={"1":"Понедельник", "2":"Вторник", "3":"Среда", "4":"Четверг", "5":"Пятница", "6":"Суббота", "7":"Воскресенье"}
@@ -62,77 +59,67 @@ async def handle(event: SimpleBotEvent) -> str:
         return message
 
 
-    #получаем аргументы 
+    # Получаем аргументы 
     args = event.object.object.message.text.split()[1:]
     
-    #Проверяем есть ли аргументы
+    # Проверяем есть ли аргументы
     if len(args) > 0:
         all_names = await storage.get(Key("ALL"))
-        #проверяем первый аргумент (имя)
+        
+        # Проверяем первый аргумент (имя)
         if (args[0] in all_names) == True:
             data = await db_find_name(args[0])
             n = 0
-            #проверяем второй аргумент (число)
+
+            # Проверяем второй аргумент (число)
             if len(args) > 1:
                 try:
                     n = int(args[1])
                 except:
                     pass
-
             
-            #получаем текст недели
-            t = week_time(n)
-            message  =readable_text(data,t)
+            # Получаем текст недели
+            message  = readable_text(data=data, time=week_time(n))
             name = "Преподователь: "
             if data.get("prefix") != None:
                 if data.get("prefix")[0] != "преподователь":
                     name = "Группа: "
-            message = name + args[0] + "\n\n" + message + "\n\nСсылка: "
+            message = name + args[0] + "\n\n" + message + "\n\nСсылка:"
             
-
-            #проверка 3 аргумента
+            # Проверка 3 аргумента (префикс для перевода)
             if len(args) > 2:
-                #проверяем есть ли такой префикс языка
                 message = translate(message, args[2])
 
-            message = message + str(data.get("url"))
-
+            message = message + " " + str(data.get("url"))
 
         else:
-            message="Данное имя не обнаружено в базе данных. Для поиска имен воспользуйтесь командой /find"
+            message = "Данное имя не обнаружено в базе данных. Для поиска имен воспользуйтесь командой /find"
     else:
-        message="Вы не ввели аргумент(имя). Для просмотра справки введите /help"
-    """         
-    #заменить time.mktime на calendar.timegm в парсере
-    week=week_now(n)
-    if data.get(week)!=None:
-        message=data.get(week)
-        message+"\n"+str(week_tests())
-    """                 
+        message = "Вы не ввели аргумент(имя). Для просмотра справки введите /help"
     await event.answer(message=message)
 
 
-#поиск имени
+# Поиск имени
 @simple_bot_message_handler(week_router, CommandsFilter(commands=("find", "поиск"), prefixes=("/")))
 async def handle(event: SimpleBotEvent) -> str:
-    #получаем аргументы 
+    # Получаем аргументы 
     args = event.object.object.message.text.split()[1:]
-    #Проверяем есть ли первый аргумент
+    # Проверяем есть ли первый аргумент
     if len(args) > 0:
         all_names = await storage.get(Key("ALL"))
-        message=""
-        #ищем подстроку в строке в массиве строк (тавтлогия)
+        message = ""
+        # Ищем подстроку в строке в массиве строк (тавтлогия)
         for x in all_names:
             if x.find(args[0]) != -1:
-                message=message+x+"\n"
-            #чтобы не получить ошибку(макс лимит сообщения), делаем не больше 500
+                message = message + x + "\n"
+            # Чтобы не получить ошибку(макс. лимит сообщения), делаем не больше 500
             if (len(message)) >= 500:
-                message=message[0:message.rfind('\n')]
+                message = message[0:message.rfind('\n')]
                 break
         if message != "":
-            message="Похожие записи в базе данных:\n"+message
+            message = "Похожие записи в базе данных:\n"+message
         else:
-            message="Похожих записей в базе данных не найдено."
+            message = "Похожих записей в базе данных не найдено."
     else:
-        message="Вы не ввели аргумент(имя). Пример команды: /find 01"
+        message = "Вы не ввели аргумент(имя). Пример команды: /find 01"
     await event.answer(message=message)
--- utils/storage.py
+++ utils/storage.py
@@ -2,13 +2,13 @@ from vkwave.bots.storage.storages import Storage
 from vkwave.bots.storage.types import Key
 from utils.web_db import do_find_all_names, do_find_all_subs
 
-#Хранилице данных, которые берутся несколько раз
-storage=Storage()
+# Хранилице данных, которые берутся несколько раз
+storage = Storage()
 async def initialize_storage():
-	#список всех групп, преподавателей
+	# Cписок всех групп, преподавателей
 	all_names = await do_find_all_names()
 	await storage.put(Key("ALL"), all_names)
-	#список подписчиков, людей, которым должно придти уведомление
+	# Cписок подписчиков, которым должно придти уведомление
 	subscribers = await do_find_all_subs()
 	await storage.put(Key("SUBS"), subscribers)
 
