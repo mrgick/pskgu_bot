de6a3b5ce235471e687340eee35a4e2403918366
mrgick <45306973+mrgick@users.noreply.github.com>
2021-08-09 13:35:01 +0300

    fix word comment

--- pskgu_bot/config.py
+++ pskgu_bot/config.py
@@ -14,7 +14,7 @@ class Config:
     SEMAPHORE = 40
     YEAR = 2021
 
-    # настрокий mongodb
+    # настроки mongo db
     DB_NAME = "DB"
     MAX_POOL_SIZE = 100
     MONGO_URI = os.environ.get('MONGO_URL')
