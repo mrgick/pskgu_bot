25dcc986f11331e10aeb3a9de3596a136c4ad5c9
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-17 19:32:57 +0300

    add logging to send messages

--- pskgu_bot/vk_bot/update.py
+++ pskgu_bot/vk_bot/update.py
@@ -11,6 +11,7 @@ async def send_updates_to_users(upd_groups):
     """
         Отсылает уведобмление пользователю об изменении группы.
     """
+    logger.info("Start sending messages")
     for group_name in upd_groups:
         users = await get_users_by_group(group_name)
         if users == []:
@@ -18,10 +19,12 @@ async def send_updates_to_users(upd_groups):
         try:
             group = await find_group_by_name(group_name)
             information = group.updated_information
+            logger.info("Sending to group="+group_name)
             if information != "":
                 for vk_id in users:
                     await vk_send_message(user_id=vk_id,
                                           message=information)
+                    logger.info("Sended message vk_id="+vk_id)
         except Exception as e:
             logger.error(e)
     logger.info(upd_groups)
