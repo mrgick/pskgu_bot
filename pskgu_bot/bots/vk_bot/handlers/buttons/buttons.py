46d5d941275c7b1bc8aaceb0d5fe40dbb678f7cb
mrgick <45306973+mrgick@users.noreply.github.com>
2021-10-23 17:27:27 +0300

    refactoring: change buttons and add new)

--- pskgu_bot/bots/vk_bot/handlers/buttons/buttons.py
+++ pskgu_bot/bots/vk_bot/handlers/buttons/buttons.py
@@ -1,12 +1,30 @@
 from ...bot import Startwith
-from .keyboards import SHOW_BUTTONS_KEYBOARD
+from .keyboards import get_show_keyboard, get_show_shifted_keyboard
 from pskgu_bot.bots.base.buttons.messages import (SHOW_MESSAGE, DELETE_MESSAGE,
-                                                  HELP_MESSAGE)
-from vkbottle import EMPTY_KEYBOARD
-from vkbottle.bot import BotLabeler, Message
+                                                  HELP_MESSAGE,
+                                                  SHOW_SHIFTED_MESSAGE)
 from pskgu_bot.bots.base.services import get_first_arg
+from pskgu_bot.bots.base.shedule import show_schedule
+from vkbottle import EMPTY_KEYBOARD
+from vkbottle.bot import BotLabeler, Message, rules
+import json
 
 bl = BotLabeler()
+
+
+@bl.message(rules.PayloadContainsRule({"command": "show"}))
+async def show_payload_handler(message: Message):
+
+    user_id = message.from_id
+    payload = json.loads(message.payload)
+    week = payload.get("week")
+    mess, _ = await show_schedule(user_id=user_id,
+                                  group_name=None,
+                                  week_shift=week,
+                                  image=False,
+                                  type_sys="vk")
+    keyb = get_show_shifted_keyboard(week)
+    await message.answer(message=mess, keyboard=keyb)
 
 
 @bl.message(Startwith(("buttons", "кнопки")))
@@ -21,7 +39,10 @@ async def buttons(message: Message):
         return mess
     elif elem == "show":
         mess = SHOW_MESSAGE
-        keyb = SHOW_BUTTONS_KEYBOARD
+        keyb = get_show_keyboard()
+    elif elem == "show_shifted":
+        mess = SHOW_SHIFTED_MESSAGE
+        keyb = get_show_shifted_keyboard(week=0)
     elif elem == "delete":
         mess = DELETE_MESSAGE
         keyb = EMPTY_KEYBOARD
--- pskgu_bot/bots/vk_bot/handlers/buttons/keyboards.py
+++ pskgu_bot/bots/vk_bot/handlers/buttons/keyboards.py
@@ -1,7 +1,33 @@
 from vkbottle import (Keyboard, KeyboardButtonColor, Text, EMPTY_KEYBOARD)
 
-show_keyboard = Keyboard(one_time=False, inline=False)
-show_keyboard.add(Text("/show"), color=KeyboardButtonColor.PRIMARY)
-show_keyboard.row()
-show_keyboard.add(Text("/show 1"), color=KeyboardButtonColor.PRIMARY)
-SHOW_BUTTONS_KEYBOARD = show_keyboard.get_json()
+
+def get_show_shifted_keyboard(week: int = 0):
+    keyboard = Keyboard(one_time=False, inline=False)
+    keyboard.add(Text("Текущая неделя",
+                      payload={
+                          "command": "show",
+                          "week": week
+                      }),
+                 color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("Предыдущая",
+                      payload={
+                          "command": "show",
+                          "week": week - 1
+                      }),
+                 color=KeyboardButtonColor.PRIMARY)
+    keyboard.add(Text("Следующая",
+                      payload={
+                          "command": "show",
+                          "week": week + 1
+                      }),
+                 color=KeyboardButtonColor.PRIMARY)
+    return keyboard.get_json()
+
+
+def get_show_keyboard():
+    keyboard = Keyboard(one_time=False, inline=False)
+    keyboard.add(Text("/show"), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("/show 1"), color=KeyboardButtonColor.PRIMARY)
+    return keyboard.get_json()
